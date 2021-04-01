a825c8dd8326bba74fa72c1f44517f7d38477805
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2021-04-01 16:02:03 -0300

    Update departamento

--- src/gui/DepartamentoListController.java
+++ src/gui/DepartamentoListController.java
@@ -1,6 +1,5 @@
 package gui;
 
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
@@ -10,6 +9,7 @@ import application.Main;
 import gui.listenrs.DataChangeListener;
 import gui.util.Alerts;
 import gui.util.Utils;
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -19,6 +19,7 @@ import javafx.fxml.Initializable;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableCell;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -29,51 +30,56 @@ import model.entidades.Departamento;
 import model.service.DepartamentoService;
 
 public class DepartamentoListController implements Initializable, DataChangeListener {
-	
+
 	private DepartamentoService service;
-	
+
 	@FXML
 	private TableView<Departamento> tableViewDepartamento;
-	
+
 	@FXML
 	private TableColumn<Departamento, Integer> tableColumnId;
-	  
+
 	@FXML
 	private TableColumn<Departamento, String> tableColumnName;
-	
+
 	@FXML
-	private Button  btNew;
-	
+	private TableColumn<Departamento, Departamento> tableColumnEdit;
+
+	@FXML
+	private Button btNew;
+
 	private ObservableList<Departamento> obsList;
-	
-	public void onBtNewAction(ActionEvent event){
-		
+
+	public void onBtNewAction(ActionEvent event) {
+
 		Stage parentStage = Utils.currentStage(event);
 		Departamento obj = new Departamento();
-		creadteDialogForm(obj,"/gui/DepartamentoForm.fxml", parentStage);
+		creadteDialogForm(obj, "/gui/DepartamentoForm.fxml", parentStage);
 	}
-	
+
 	@Override
 	public void initialize(URL uri, ResourceBundle rb) {
 		// TODO Auto-generated method stub
 		inicializeNodes();
-		
-		
+
 	}
+
 	// ingeçao de depêndencia com a classe departamentoService.
-	public void setDepartamentoService (DepartamentoService service) {
+	public void setDepartamentoService(DepartamentoService service) {
 		this.service = service;
 	}
-	// usando o serviço 
+
+	// usando o serviço
 	public void updateTableView() {
-		if(service == null) {
+		if (service == null) {
 			throw new IllegalStateException("Serviço Nulo");
 		}
 		List<Departamento> list = service.findAll();
 		obsList = FXCollections.observableArrayList(list);
 		tableViewDepartamento.setItems(obsList);
+		initEditButtons();
 	}
-		
+
 	private void inicializeNodes() {
 		tableColumnId.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnName.setCellValueFactory(new PropertyValueFactory<>("Nome"));
@@ -81,8 +87,9 @@ public class DepartamentoListController implements Initializable, DataChangeList
 		Stage stage = (Stage) Main.getMainScene().getWindow();
 		tableViewDepartamento.prefHeightProperty().bind(stage.heightProperty());
 	}
+
 	// ação de formulário de cadastro de derpatamentos
-	private void creadteDialogForm (Departamento obj, String absoltename, Stage parentStage) {
+	private void creadteDialogForm(Departamento obj, String absoltename, Stage parentStage) {
 		try {
 			// carregando a view de novo de partamento.
 			FXMLLoader loader = new FXMLLoader(getClass().getResource(absoltename));
@@ -93,8 +100,8 @@ public class DepartamentoListController implements Initializable, DataChangeList
 			controller.setDepartamentoService(new DepartamentoService());
 			controller.subscribeDataChangeListener(this);
 			controller.updateFormData();
-			//um palco na frente do palco
-			
+			// um palco na frente do palco
+
 			Stage dialogStage = new Stage();
 			// chamada da nova janela
 			dialogStage.setTitle("Dados do Novo Departamento");
@@ -103,8 +110,8 @@ public class DepartamentoListController implements Initializable, DataChangeList
 			dialogStage.initOwner(parentStage);
 			dialogStage.initModality(Modality.WINDOW_MODAL);
 			dialogStage.showAndWait();
-			
-		}catch (IOException e) {
+
+		} catch (IOException e) {
 			Alerts.showAlert("IOException", "Erro londging View", e.getMessage(), AlertType.ERROR);
 		}
 	}
@@ -112,7 +119,27 @@ public class DepartamentoListController implements Initializable, DataChangeList
 	@Override
 	public void onDataChanged() {
 		updateTableView();
-		
+
+	}
+
+	// metodo de edição de cada linha com o departamento existente
+	private void initEditButtons() {
+		tableColumnEdit.setCellValueFactory(param -> new ReadOnlyObjectWrapper<>(param.getValue()));
+		tableColumnEdit.setCellFactory(param -> new TableCell<Departamento, Departamento>() {
+			private final Button button = new Button("Editar");
+
+			@Override
+			protected void updateItem(Departamento obj, boolean empty) {
+				super.updateItem(obj, empty);
+				if (obj == null) {
+					setGraphic(null);
+					return;
+				}
+				setGraphic(button);
+				button.setOnAction(
+						event -> creadteDialogForm(obj, "/gui/DepartamentoForm.fxml", Utils.currentStage(event)));
+			}
+		});
 	}
 
 }
